fafb159e52cb55c93fd5f48447dcb7996788d201
kunkkaliu <zudapang@qq.com>
2020-01-23 15:46:04 +0800

    fix: 备案号

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -3,6 +3,6 @@ import './index.less';
 
 export default function Footer() {
     return (
-        <a href="http://beian.miit.gov.cn" className="footer">京ICP备19053773号-1</a>
+        <a href="http://beian.miit.gov.cn" className="footer">京ICP备19053773号</a>
     );
 }
